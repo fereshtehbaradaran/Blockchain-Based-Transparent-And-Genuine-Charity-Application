0483ea8c31f471ac6bd45e7cb48c39680962c89d
aydaZaman <ayda.zaman1058@gmail.com>
2022-11-29 22:39:08 -0600

    token_erc20

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    loc_development_development: {
+      network_id: "*",
+      port: 8545,
+      host: "127.0.0.1"
+    }
+  },
+  mocha: {},
+  compilers: {
+    solc: {
+      version: "0.8.17"
+    }
+  }
+};
