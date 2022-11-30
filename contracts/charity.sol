15af6a659607ceb2d5d14cb7c54c7a3154316113
aydaZaman <ayda.zaman1058@gmail.com>
2022-11-30 12:02:48 -0600

    donar+charity

--- contracts/charity.sol
+++ contracts/charity.sol
@@ -1,2 +1,50 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+contract Charity{
+
+struct charityOrganization{
+  string charityName;
+  address charityAddress;
+  string Desc;
+}
+charityOrganization public charity;
+
+struct Donator{
+    address donatorAddress;
+    string donarname;
+    //amount of donation
+    uint amount;
+    //which project?
+    uint projectId;
+
+
+}
+Donator[] public donators;
+
+constructor(){
+      charity = charityOrganization("charityOf3Musketry",msg.sender,"Genuine Charity App");
+      
+}
+
+function transfor(address form ,address to,uint amount)public payable returns(bool){
+    if(msg.value>=amount){
+     //transfer
+     return true;
+    }
+    return false;
+
+}
+function directDonation(string memory name ,uint amount, uint pId)public{
+    Donator memory newD= Donator(msg.sender,name, amount, pId);
+    if(true==transfor(msg.sender, charity.charityAddress,amount)){
+        donators.push(newD);
+    }
+
+}
+function voitingDonation()public{
+
+}
+
+
+}
